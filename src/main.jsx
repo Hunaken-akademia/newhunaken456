678311a76ad178f6cc3f34435ea68486d54efd86
Hunaken-akademia <aru020940@gmail.com>
2026-07-26 21:44:35 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("root要素が見つかりません");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
